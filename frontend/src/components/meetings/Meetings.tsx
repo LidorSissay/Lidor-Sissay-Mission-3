ec2884e1e3b6ca38c144c1231dc4f4c795056be3
LidorSissay <lidsis96w@gmail.com>
2026-06-09 10:56:17 +0300

    display meetings by selected team

--- frontend/src/components/meetings/Meetings.tsx
+++ frontend/src/components/meetings/Meetings.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import type DevelopmentTeam from "../../models/DevelopmentTeam";
+import type Meeting from "../../models/Meeting";
+
 import { getDevelopmentTeams } from "../../services/development-teams";
+import { getMeetingsByTeam } from "../../services/meetings";
 
 export default function Meetings() {
 
     const [teams, setTeams] = useState<DevelopmentTeam[]>([])
+    const [meetings, setMeetings] = useState<Meeting[]>([])
 
     useEffect(() => {
 
@@ -14,12 +18,33 @@ export default function Meetings() {
 
     }, [])
 
+    async function handleTeamChange(event: React.ChangeEvent<HTMLSelectElement>) {
+
+        const teamId = event.target.value
+
+        if (!teamId) {
+            setMeetings([])
+            return
+        }
+
+        try {
+
+            const meetings = await getMeetingsByTeam(teamId)
+
+            setMeetings(meetings)
+
+        } catch (err) {
+            console.error(err)
+        }
+
+    }
+
     return (
         <div className="Meetings">
 
             <h1>Meetings</h1>
 
-            <select>
+            <select onChange={handleTeamChange}>
                 <option value="">
                     Select Team
                 </option>
@@ -36,6 +61,30 @@ export default function Meetings() {
                 }
 
             </select>
+
+            <hr />
+
+            {
+                meetings.map(meeting => (
+                    <div key={meeting.id}>
+
+                        <h3>{meeting.description}</h3>
+
+                        <p>
+                            Room: {meeting.roomName}
+                        </p>
+
+                        <p>
+                            Start: {meeting.startDateTime}
+                        </p>
+
+                        <p>
+                            End: {meeting.endDateTime}
+                        </p>
+
+                    </div>
+                ))
+            }
 
         </div>
     )
