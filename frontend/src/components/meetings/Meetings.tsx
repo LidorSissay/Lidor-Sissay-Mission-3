624c5b2353f84161fde46ead5bc7bac6fda1ab8b
LidorSissay <lidsis96w@gmail.com>
2026-06-09 10:51:18 +0300

    load development teams into meetings page

--- frontend/src/components/meetings/Meetings.tsx
+++ frontend/src/components/meetings/Meetings.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import type DevelopmentTeam from "../../models/DevelopmentTeam";
+import { getDevelopmentTeams } from "../../services/development-teams";
+
 export default function Meetings() {
+
+    const [teams, setTeams] = useState<DevelopmentTeam[]>([])
+
+    useEffect(() => {
+
+        getDevelopmentTeams()
+            .then(setTeams)
+            .catch(console.error)
+
+    }, [])
+
     return (
         <div className="Meetings">
+
             <h1>Meetings</h1>
+
+            <select>
+                <option value="">
+                    Select Team
+                </option>
+
+                {
+                    teams.map(team => (
+                        <option
+                            key={team.id}
+                            value={team.id}
+                        >
+                            {team.name}
+                        </option>
+                    ))
+                }
+
+            </select>
+
         </div>
     )
 }
